52d756f9ed3a151c1937e3e56686515207d8b65e
Andriuhilario <149027765+Andriuhilario@users.noreply.github.com>
2024-01-25 17:12:41 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-/*
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do numero secreto'
-
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um numero de 1 a 10:';
-*/
-
 let listaNumerosSorteados = [];
 let numeroLimite = 10;
 let numeroSecreto = gerarUmNumeroAleatorio();
